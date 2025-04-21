0e62e96f84e1a7e8a4bb8a9c6bb1e20a9f14f5a9
VIGHNESH M S <princevighnesh71@gmail.com>
2025-04-21 21:34:03 +0530

    adding multi language masking

--- utils/pii_masker.py
+++ utils/pii_masker.py
@@ -1,17 +1,38 @@
 import spacy
 import re
 
-nlp = spacy.load("en_core_web_sm")
+nlp = spacy.load("xx_ent_wiki_sm")
 
 def mask_pii(text):
     doc = nlp(text)
-    masked = text
+    masked_text = text
+    
     for ent in doc.ents:
-        if ent.label_ in ['PERSON', 'ORG', 'GPE', 'DATE', 'EMAIL', 'PHONE']:
-            masked = masked.replace(ent.text, f"{{{{{ent.label_}}}}}")
-    return masked
+        if ent.label_ in ['PERSON', 'ORG', 'GPE', 'DATE', 'Email', ]:
+            masked_text = masked_text.replace(ent.text, f"{{{{{ent.label_}}}}}")
+
+    masked_text = re.sub(r'\b[\w.-]+?@\w+?\.\w+?\b', '{{EMAIL}}', masked_text)
+    phone_pattern = re.compile(
+        r'(\+?\d{1,3}[-\s]?)?(\(?\d{1,4}\)?[-\s]?)\d{3,4}[-\s]?\d{4}'
+    )
+    text = phone_pattern.sub('{{PHONE}}', masked_text)
+    return (text)
+# test the function
 
 
-text = "Seeking information on digital strategies that can aid in brand growth and details on the available services. Looking forward to learning more to help our business grow My name is Elena Ivanova.. Thank you, and I look forward to hearing from you soon. You can reach me at fatima.farsi@help.com."
+text = '''Subject: Änderungsantrag zur Abrechnungsstruktur
 
-print(mask_pii(text))
+Sehr geehrter Kundenservice, 
+
+ich schreibe, um eine Änderung in der Abrechnungsstruktur für den AWS-Management-Service anzufordern, wobei meine Kontonummer <acc_num> ist. Da wir derzeit einen Anstieg der Nutzung während unserer Bereitstellungsphasen erleben, benötigen wir ein Abrechnungsmodell, das diese erweiterte Nutzung besser berücksichtigen kann My name is Sophia Rossi.. Derzeit haben unsere Kosten die üblichen Grenzen überschritten, was sich erheblich auf unser Budget auswirkt. 
+
+Für alle notwendigen Diskussionen oder Klarstellungen kontaktieren Sie mich bitte unter <tel_num> oder per E-Mail an <email>. Ich freue mich auf Ihre Unterstützung bei der Anpassung unseres Vertrags, um unseren spezifischen Anforderungen während dieser intensiven Bereitstellungsphasen gerecht zu werden.
+
+Vielen Dank.
+
+Mit freundlichen Grüßen,
+<name> You can reach me at fatima.farsi@help.com.'''
+
+x = mask_pii(text)
+print(x)
+  # prints the count of each type of PII
