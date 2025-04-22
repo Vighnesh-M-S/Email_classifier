688f77951848255af3026d1148aceba88a0fb3b1
VIGHNESH M S <princevighnesh71@gmail.com>
2025-04-22 21:26:56 +0530

    preprocess

--- utils/pii_masker.py
+++ utils/pii_masker.py
@@ -4,19 +4,23 @@ import re
 nlp = spacy.load("xx_ent_wiki_sm")
 
 def mask_pii(text):
+    
+    text = re.sub(r'\b[\w.-]+?@\w+?\.\w+?\b', '{{EMAIL}}', text)
+    phone_pattern = re.compile(
+        r'(\+?\d{1,3}[-\s]?)?(\(?\d{1,4}\)?[-\s]?)\d{3,4}[-\s]?\d{4}'
+    )
+    text = phone_pattern.sub('{{PHONE}}', text)
+
     doc = nlp(text)
     masked_text = text
     
     for ent in doc.ents:
         if ent.label_ in ['PERSON', 'ORG', 'GPE', 'DATE', 'Email', ]:
-            masked_text = masked_text.replace(ent.text, f"{{{{{ent.label_}}}}}")
+            if not re.match(r'\{\{.*?\}\}', ent.text):
+                masked_text = masked_text.replace(ent.text, f"{{{{{ent.label_}}}}}")
 
-    masked_text = re.sub(r'\b[\w.-]+?@\w+?\.\w+?\b', '{{EMAIL}}', masked_text)
-    phone_pattern = re.compile(
-        r'(\+?\d{1,3}[-\s]?)?(\(?\d{1,4}\)?[-\s]?)\d{3,4}[-\s]?\d{4}'
-    )
-    text = phone_pattern.sub('{{PHONE}}', masked_text)
-    return (text)
+
+    return (masked_text)
 # test the function
 
 
