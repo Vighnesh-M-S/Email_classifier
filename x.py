156fb65353ba4edffb71ec8309c9954a74d825f2
VIGHNESH M S <58000117+Vighnesh-M-S@users.noreply.github.com>
2025-04-24 16:34:38 +0530

    Delete x.py

--- x.py
+++
@@ -1,5 +0,0 @@
-import spacy
-
-# Directly load the model from the path if not recognized by the package
-nlp = spacy.load("en_core_web_sm")
-print(nlp("My name is Elon Musk and I work at SpaceX.").ents)
