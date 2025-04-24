c40503a1a6d7f1423302492eab0f632ce98d837e
VIGHNESH M S <58000117+Vighnesh-M-S@users.noreply.github.com>
2025-04-24 16:35:54 +0530

    Delete models 4.12.36 PM directory

--- models 4.12.36 PM/models.py
+++
@@ -1,8 +0,0 @@
-import pickle
-
-def load_model_tokenizer(model_path="models/email_classifier.pkl", tokenizer_path="models/tfidf_vectorizer.pkl"):
-    with open(model_path, "rb") as f:
-        model = pickle.load(f)
-    with open(tokenizer_path, "rb") as f:
-        tokenizer = pickle.load(f)
-    return model, tokenizer
